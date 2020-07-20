9456379059a0282fc77ce5d72fe281ead065957b
AlvenZhang <1253487784@qq.com>
2020-07-20 16:49:42 +0800

    suspend

---
+++ cloud-consumerzipkin-order80/src/main/java/com/atguigu/springcloud/ZipkinOrderMain80.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinOrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinOrderMain80.class,args);
+    }
+}
---
+++ cloud-consumerzipkin-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin()
+    {
+        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
+        return result;
+    }
+}
